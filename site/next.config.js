3a2c8859eea7eaf55aaf44d3fa84ae983dacf97d
momoji123 <77244643+momoji123@users.noreply.github.com>
2023-12-24 21:05:01 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Oji Kasm Workspaces',
+    description: 'My private kasm workspace.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://momoji123.github.io/workspaces_registry/',
+    contactUrl: 'https://github.com/momoji123/workspaces_registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/workspaces_registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
