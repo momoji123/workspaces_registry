7748f5f21e56cbbfa639aafff7e04ce67d7a9374
momoji123 <77244643+momoji123@users.noreply.github.com>
2024-10-06 19:56:44 +0200

    Update next.config.js
    # set as 1.0, it will automatically updated

--- site/next.config.js
+++ site/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/workspaces_registry/1.1',
+  basePath: '/workspaces_registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
